5c0f8a2ba6b8914e428bf3bc9b9056b645a32512
K. R. McDonald <152812280+KRMcDonald@users.noreply.github.com>
2024-01-07 15:55:12 -0500

    Add files via upload

---
+++ day003-theworsttextadventure.py
@@ -0,0 +1,45 @@
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.") 
+
+#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
+
+#Write your code below this line 👇
+direction = input("You come to a cross roads. Do you go left or right? ").lower()
+if direction == "left":
+  river = input("You come to a river. You can try to swim, or wait for a boat. Do you swim or wait? ").lower()
+  if river == "wait":
+    door = input("A boat appears and offers to carry you downstream. You come to a shed with three doors: red, yellow, and blue. Which color door do you open? ").lower()
+    if door == "yellow":
+      print("You find the treasure and win!!1")
+    elif door == "red":
+      print("There is a fire and you are burned to death and die. Game over.")
+    elif door == "blue":
+      print("Ravenous beasts attack you. You die. Game over.")
+    else:
+      print("That's not a door color. You have a heart attack. And die. Game over.")
+  else:
+    print("You are attacked by trout and die. Game over.")
+else:
+  print("You walk into a sinkhole and die. Game over.")
