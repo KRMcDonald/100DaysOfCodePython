7cc8fbae6ca407c5ebbeb671885b2cffdbdc1eed
K. R. McDonald <152812280+KRMcDonald@users.noreply.github.com>
2024-01-22 18:05:13 -0500

    Add files via upload

---
+++ day011-simplified-blackjack.py
@@ -0,0 +1,143 @@
+#I think I'm more confused by Blackjack than Python right now...
+
+############### Blackjack Project #####################
+
+#Difficulty Normal 😎: Use all Hints below to complete the project.
+#Difficulty Hard 🤔: Use only Hints 1, 2, 3 to complete the project.
+#Difficulty Extra Hard 😭: Only use Hints 1 & 2 to complete the project.
+#Difficulty Expert 🤯: Only use Hint 1 to complete the project.
+
+############### Our Blackjack House Rules #####################
+
+## The deck is unlimited in size. 
+## There are no jokers. 
+## The Jack/Queen/King all count as 10.
+## The the Ace can count as 11 or 1.
+## Use the following list as the deck of cards:
+## cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+## The cards in the list have equal probability of being drawn.
+## Cards are not removed from the deck as they are drawn.
+## The computer is the dealer.
+
+#My code
+import random
+from art import logo
+print(logo)
+cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+
+def draw_card():
+  return cards[random.randint(0,len(cards) - 1)]
+
+def hand_sum(hand):
+  total = 0
+  for card in hand:
+    total += card
+  return total
+
+def hit(hand):
+  hand.append(draw_card())
+  return hand
+
+def ace_function(cards):
+  if 11 in cards:
+    for card in cards:
+      if card == 11:
+        card = 1
+
+players_cards = []
+dealers_cards = []
+
+players_cards.append(draw_card())
+players_cards.append(draw_card())
+players_sum = hand_sum(players_cards)
+
+print(f"Your cards: {players_cards}")
+print(f"Your total: {players_sum}")
+
+dealers_cards.append(draw_card())
+print(f"Dealer's cards: {dealers_cards} and [face down]")
+
+hit_choice = input("Hit? y/n ")
+while hit_choice == "y":
+  players_cards = hit(players_cards)
+  players_sum = hand_sum(players_cards)
+  print(f"Your cards: {players_cards}")
+  print(f"Your total: {players_sum}")
+  hit_choice = input("Hit? y/n ")
+print(f"Your cards: {players_cards}")
+print(f"Your total: {players_sum}")
+
+dealers_cards = hit(dealers_cards)
+dealers_sum = hand_sum(dealers_cards)
+while dealers_sum < 17:
+  dealers_cards = hit(dealers_cards)
+  dealers_sum = hand_sum(dealers_cards)
+print(f"Dealer's cards: {dealers_cards}")
+print(f"Dealer's total: {dealers_sum}")
+
+#else_function() shorten the below
+def else_function():
+  if players_sum > dealers_sum:
+    print("You win.")
+  elif players_sum == dealers_sum:
+    print("Draw.")
+  else:
+    print("You lose.")
+
+if players_sum > 21:
+  players_cards = ace_function(players_cards)
+  if hand_sum(players_cards) > 21:
+    print("You lose.")
+  else:
+    else_function()
+elif dealers_sum > 21:
+  dealers_cards = ace_function(dealers_cards)
+  if hand_sum(dealers_cards) > 21:
+    print("You win.")
+  else:
+    else_function()
+else:
+  else_function()
+
+##################### Hints #####################
+
+#Hint 1: Go to this website and try out the Blackjack game: 
+#   https://games.washingtonpost.com/games/blackjack/
+#Then try out the completed Blackjack project here: 
+#   http://blackjack-final.appbrewery.repl.run
+
+#Hint 2: Read this breakdown of program requirements: 
+#   http://listmoz.com/view/6h34DJpvJBFVRlZfJvxF
+#Then try to create your own flowchart for the program.
+
+#Hint 3: Download and read this flow chart I've created: 
+#   https://drive.google.com/uc?export=download&id=1rDkiHCrhaf9eX7u7yjM1qwSuyEk-rPnt
+
+#Hint 4: Create a deal_card() function that uses the List below to *return* a random card.
+#11 is the Ace.
+#cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+
+#Hint 5: Deal the user and computer 2 cards each using deal_card() and append().
+#user_cards = []
+#computer_cards = []
+
+#Hint 6: Create a function called calculate_score() that takes a List of cards as input 
+#and returns the score. 
+#Look up the sum() function to help you do this.
+
+#Hint 7: Inside calculate_score() check for a blackjack (a hand with only 2 cards: ace + 10) and return 0 instead of the actual score. 0 will represent a blackjack in our game.
+
+#Hint 8: Inside calculate_score() check for an 11 (ace). If the score is already over 21, remove the 11 and replace it with a 1. You might need to look up append() and remove().
+
+#Hint 9: Call calculate_score(). If the computer or the user has a blackjack (0) or if the user's score is over 21, then the game ends.
+
+#Hint 10: If the game has not ended, ask the user if they want to draw another card. If yes, then use the deal_card() function to add another card to the user_cards List. If no, then the game has ended.
+
+#Hint 11: The score will need to be rechecked with every new card drawn and the checks in Hint 9 need to be repeated until the game ends.
+
+#Hint 12: Once the user is done, it's time to let the computer play. The computer should keep drawing cards as long as it has a score less than 17.
+
+#Hint 13: Create a function called compare() and pass in the user_score and computer_score. If the computer and user both have the same score, then it's a draw. If the computer has a blackjack (0), then the user loses. If the user has a blackjack (0), then the user wins. If the user_score is over 21, then the user loses. If the computer_score is over 21, then the computer loses. If none of the above, then the player with the highest score wins.
+
+#Hint 14: Ask the user if they want to restart the game. If they answer yes, clear the console and start a new game of blackjack and show the logo from art.py.
+
